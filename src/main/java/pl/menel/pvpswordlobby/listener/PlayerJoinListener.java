8b93db4dccfcd0cb29588bf94c22df696a8efefd
danielo535 <79422069+danielo535@users.noreply.github.com>
2023-08-23 03:13:56 +0200

    add PlayerJoinListener.java

---
+++ src/main/java/pl/menel/pvpswordlobby/listener/PlayerJoinListener.java
@@ -0,0 +1,17 @@
+package pl.menel.pvpswordlobby.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import pl.menel.pvpswordlobby.manager.CombatManager;
+
+public class PlayerJoinListener implements Listener {
+    private final CombatManager combatManager;
+    public PlayerJoinListener(CombatManager combatManager) {
+        this.combatManager = combatManager;
+    }
+    @EventHandler
+    public void onJoinServer(PlayerJoinEvent event) {
+        combatManager.onPlayerPvP(event.getPlayer(), false);
+    }
+}
